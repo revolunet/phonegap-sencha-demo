5dcb0e39f87eacdac37174c109973060277275b6
Julien Bouquillon <julien@bouquillon.com>
2012-04-12 00:00:48 +0200

    rename folders, update android cordova to 1.6

--- exampleapp/bin/cordovaExample-debug-unaligned.apk.d
+++
@@ -1,6 +0,0 @@
-/Users/juju/Documents/projects/phonegap-sencha-android/exampleapp/bin/cordovaExample-debug-unaligned.apk : \
-/Users/juju/Documents/projects/phonegap-sencha-android/exampleapp/bin/cordovaExample.ap_ \
-/Users/juju/Documents/projects/phonegap-sencha-android/exampleapp/bin/classes.dex \
-/Users/juju/Documents/projects/phonegap-sencha-android/exampleapp/src/org/apache/cordova/example/cordovaExample.java-e \
-/Applications/android-sdk-mac_x86/tools/support/annotations.jar \
-/Users/juju/Documents/projects/phonegap-sencha-android/exampleapp/libs/cordova-1.5.0.jar \
